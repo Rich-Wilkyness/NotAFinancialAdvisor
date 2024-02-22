dd2d7f2f58b2aa65059015bc503d1f0b5507a5cb
Rich-Wilkyness <r.christian.wilkerson@gmail.com>
2024-02-22 11:53:29 -0700

    trying to fix auth

--- not-a-financial-advisor/next.config.js
+++ not-a-financial-advisor/next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-module.exports = nextConfig;
+module.exports = {
+    webpack: (config, { isServer }) => {
+      // Add rule for HTML files
+      config.module.rules.push({
+        test: /\.html$/,
+        use: 'html-loader',
+      });
+  
+      // Return updated configuration
+      return config;
+    },
+  };
